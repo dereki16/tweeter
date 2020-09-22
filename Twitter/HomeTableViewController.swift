91132b7c1d77ef6c23002c1a6f4816c95b00d57b
dereki16 <derekiniguez@gmail.com>
2020-09-22 06:06:23 -0700

    init login + logout (not logging in and home table view controller not connecting to storyboard)

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Derek Iniguez on 9/22/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UIViewController {
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
